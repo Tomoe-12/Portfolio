2b123c5485f78dd2aa64065cbcae76c5e2d7a626
Tomoe-12 <li8993han@gmail.com>
2024-06-10 07:48:53 +0630

    adding work experience done but need to fix it for my personal may be later

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero.jsx'
 import Skills from './components/Skills/Skills.jsx'
+import WorkExperience from './WorkExperience/WorkExperience.jsx'
 function App() {
   return (
     <>
       <Navbar />
       <div className='container'>
         <Hero />
-        <Skills/>
+        <Skills />
+        <WorkExperience />
       </div>
     </>
   )
--- src/utils/data.js
+++ src/utils/data.js
@@ -5,10 +5,10 @@ export const SKILLS = [
         title: 'Frontend',
         icon: Assests.frontend,
         skills: [
-            { skill: 'HTML5', percentage: '80%' },
-            { skill: 'CSS', percentage: '90%' },
-            { skill: 'Javascript', percentage: '75%' },
-            { skill: 'React.js', percentage: '80%' },
+            { skill: 'HTML5', percentage: '70%' },
+            { skill: 'CSS', percentage: '80%' },
+            { skill: 'Javascript', percentage: '65%' },
+            { skill: 'React.js', percentage: '60%' },
         ],
     },
     {
@@ -39,4 +39,37 @@ export const SKILLS = [
         ],
     },
 
+]
+
+export const WORK_EXPERIENCE = [
+    {
+        title : 'Senior frontend Developer at Tech Inoovators',
+        date : 'January 2022 - Present',
+        responsibilities : [
+            'Meeting with the development team to discuss user interface ideas and applications.',
+            'Developing and implementing highly-responsive user interface components.',
+            'Writing application interface codes using JavaScript, following libraries such as React.js or Angular.',
+            'Developing and implementing front-end architecture to support user interface concepts.'
+        ]
+    },
+    {
+        title : 'Junior Web Developer at Digital Solution',
+        date : 'June 2020 - December Present',
+        responsibilities : [
+            'Meeting with the development team to discuss user interface ideas and applications.',
+            'Developing and implementing highly-responsive user interface components.',
+            'Writing application interface codes using JavaScript, following libraries such as React.js or Angular.',
+            'Developing and implementing front-end architecture to support user interface concepts.'
+        ]
+    },
+    {
+        title : 'Frontend Intern at Web Studio',
+        date : 'May 2019 - August 2019',
+        responsibilities : [
+            'Meeting with the development team to discuss user interface ideas and applications.',
+            'Developing and implementing highly-responsive user interface components.',
+            'Writing application interface codes using JavaScript, following libraries such as React.js or Angular.',
+            'Developing and implementing front-end architecture to support user interface concepts.'
+        ]
+    }
 ]
